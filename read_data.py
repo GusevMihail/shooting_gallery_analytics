1e7518df0d62cd6f95c44a84c2c091e4297efa55
Mihail <gusev.mihail.34@gmail.com>
2020-01-15 16:59:56 +0300

    process numerical and categorical fields

--- read_data.py
+++ read_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def is_numeric_field(field_name: str):
     int_factor_names = ('cash', 'toys', 'mood', 'power_reserve', 'plan',
-                        'exchange', 'collection', 'prepayment,', 'balance', 'are_you_good', 'good_day')
+                        'exchange', 'collection', 'prepayment', 'balance', 'are_you_good', 'good_day')
     for template in int_factor_names:
         if template in field_name:
             return True
@@ -16,6 +16,18 @@ def is_categorical_field(field_name: str):
     return field_name in cat_factor_names
 
 
+def is_list_field(field_name: str):
+    list_factor_names = ('ok_guns', 'bed_guns')
+    return field_name in list_factor_names
+
+
+def str_to_num(field: str):
+    if field == '' or field is None:
+        return 0
+    else:
+        return int(field)
+
+
 def categorical_to_num(field: str):
     empty_field_value = -1
     convert_dict = {
@@ -25,7 +37,7 @@ def categorical_to_num(field: str):
         'хватает': 3,
         'много': 4}
 
-    if field == '':
+    if field == '' or field is None:
         return empty_field_value
     for pattern in convert_dict.keys():
         if pattern in field.lower():
@@ -34,15 +46,15 @@ def categorical_to_num(field: str):
 
 def process_fields(line):
     for field in line:
-        line[field] = line[field][0]
-        if is_numeric_field(field):
-            if line[field] == '':
-                line[field] = 0
-            else:
-                line[field] = int(line[field])
+        if is_list_field(field):
+            continue
         else:
-            if is_categorical_field(field):
-                line[field] = categorical_to_num(line[field])
+            line[field] = line[field][0]  # explode single-element lists
+        if is_numeric_field(field):
+            line[field] = str_to_num(line[field])
+            continue
+        if is_categorical_field(field):
+            line[field] = categorical_to_num(line[field])
 
 
 def process_line(line):
